0b022e417c5dcdbc13f5b3598b2305ca2915d022
João Monteiro <monteirojdev@gmail.com>
2026-08-13 10:41:20 -0300

    Remove directory structure comments from main.py

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
-README.md
-│
-├── .gitignore
-├── README.md
-├── requirements.txt
-│
-├── src/
-│   ├── __init__.py
-│   ├── main.py
-│   ├── api.py
-│   └── interface.py
-│
-└── assets/
-    └── preview.png
+
